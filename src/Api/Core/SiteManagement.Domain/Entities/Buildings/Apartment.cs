cc7e673289453bc48b71412b4778391522cc702c
Mehmet Şimşek <mtsmsek@gmail.com>
2024-02-11 23:55:52 +0300

    Add project files.

---
+++ src/Api/Core/SiteManagement.Domain/Entities/Buildings/Apartment.cs
@@ -0,0 +1,18 @@
+﻿using SiteManagement.Domain.Entities.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteManagement.Domain.Entities.Buildings;
+
+public class Apartment : BaseEntity
+{
+    public Guid BlockId { get; set; }
+    public bool Status { get; set; }
+    //TODO create enumaration for apartment type
+    public int ApartmentNumber { get; set; }
+    public int FloorNumber { get; set; }
+    public bool IsTenant { get; set; }
+}
---
+++ src/Api/Core/SiteManagement.Domain/Primitives/Enumaration.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteManagement.Domain.Primitives;
+
+public abstract class Enumaration<TEnum> : IEquatable<Enumaration<TEnum>>
+    where TEnum : Enumaration<TEnum>
+{
+    private static readonly Dictionary<int, TEnum> Enumarations = CreateEnumarations();
+    protected Enumaration(int value, string name) 
+    {
+        Value = value;
+        Name = name;
+    }
+
+    public int Value { get; protected init; }
+    public string Name { get; protected init; } = string.Empty;
+    public static TEnum? FromValue(int value)
+    {
+        return Enumarations.TryGetValue(value,
+                                        out TEnum? enumaration) ?
+                                        enumaration :
+                                        default;
+    }
+    public static TEnum? FromName(string name)
+    {
+        return Enumarations.Values
+               .SingleOrDefault(e => e.Name == name);
+    }
+    public bool Equals(Enumaration<TEnum>? other)
+    {
+        if(other is null)
+           return false;
+
+        return GetType() == other.GetType() &&
+               Value == other.Value;
+
+    }
+    public override bool Equals(object? obj)
+    {
+        return obj is Enumaration<TEnum> other &&
+               Equals(other);
+
+    }
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+    public override string ToString()
+    {
+        return Name;
+    }
+    private static Dictionary<int,TEnum> CreateEnumarations()
+    {
+        var enumerationType = typeof(TEnum);
+
+        var fieldsForType = enumerationType
+            .GetFields(
+                        BindingFlags.Public |
+                        BindingFlags.Static |
+                        BindingFlags.FlattenHierarchy)
+            .Where(fieldInfo =>
+                   enumerationType.IsAssignableFrom(fieldInfo.FieldType))
+            .Select(fieldInfo => 
+                    (TEnum)fieldInfo.GetValue(default)!);
+
+        return fieldsForType.ToDictionary(x => x.Value);
+    }
+}
